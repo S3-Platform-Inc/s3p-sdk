ca1b0d1391fdf04ef9605031d2dfb8185fb9ded3
CuberHuber <lupashcko.rom@yandex.ru>
2024-11-18 11:41:02 +0300

    Add assert to check no positive max_documents value at S3PParserBase class
    Fixed: #7

---
+++ tests/plugin/payloads/fixtures/refers.py
@@ -0,0 +1,25 @@
+import pytest
+
+from s3p_sdk.types import S3PRefer
+from s3p_sdk.plugin.config.type import SOURCE, ML
+
+
+def refers(_type: str) -> S3PRefer:
+    """
+
+    :param _type:
+    :return:
+    """
+    return {
+            SOURCE: S3PRefer(1, "test ref 1", SOURCE, None),
+            ML: S3PRefer(2, "test ref 2", ML, None),
+        }.get(_type)
+
+
+@pytest.fixture
+def src_refer() -> S3PRefer:
+    """
+
+    :return:
+    """
+    return refers(SOURCE)
--- tests/plugin/payloads/test_make_parser.py
+++ tests/plugin/payloads/test_make_parser.py
@@ -4,21 +4,40 @@ import pytest
 import datetime
 
 from s3p_sdk.plugin.payloads.parsers import S3PParserBase
-from s3p_sdk.types import S3PRefer
+from s3p_sdk.types import S3PRefer, S3PDocument
+from s3p_sdk.plugin.config.type import SOURCE, ML
+
+from tests.plugin.payloads.fixtures.refers import src_refer
 
 
 class TestParserMaker:
 
     class MyParser(S3PParserBase):
 
-        def __init__(self, refer: S3PRefer, ):
-            super().__init__(refer)
+        def __init__(self, refer: S3PRefer, max_docs: int = None, last_doc: S3PDocument = None):
+            super().__init__(refer, max_docs, last_doc)
 
         def _parse(self) -> None:
             # HOST - это главная ссылка на источник, по которому будет "бегать" парсер
             self.logger.debug(F"Parser enter to {self._refer.to_logging}")
 
-    def test_make_parser(self, caplog):
-        ref = S3PRefer(1, "test ref 1", "SOURCE", None)
+    def test_make_parser(self, src_refer):
+        """Simple parser init"""
+        m = self.MyParser(src_refer)
 
-        m = self.MyParser(ref)
+    def test_init_with_valid_max_docs(self, src_refer):
+        """Simple parser init with valid max_docs"""
+        assert self.MyParser(src_refer, max_docs=2)
+
+    def test_init_with_null_max_docs(self, src_refer):
+        """
+        Simple parser init with zero value max_docs.
+        Max_documents must be positive integer or None value.
+        """
+        try:
+            self.MyParser(src_refer, max_docs=0)
+        except AssertionError as e:
+            assert True, e
+        else:
+            assert False, "max_documents must be positive integer or None value"
+
