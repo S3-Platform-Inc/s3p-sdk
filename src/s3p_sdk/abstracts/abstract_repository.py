19eeb08377c3c02e5935cd003aea2b65878ad41a
CuberHuber <lupashcko.rom@yandex.ru>
2025-04-02 14:49:11 +0300

    fix(code): to v0.2.12dev4 (#37)
    - add two methods to retrieve absolute path

--- src/s3p_sdk/abstracts/abstract_repository.py
+++ src/s3p_sdk/abstracts/abstract_repository.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
+
 from multipledispatch import dispatch
 
 from s3p_sdk.types import S3PPlugin, S3PDocument
@@ -19,4 +21,12 @@ class AbstaractRepository(ABC):
 
     @abstractmethod
     def open(self, document: S3PDocument, filename: str, **kwargs):
+        ...
+
+    @abstractmethod
+    def dir_of(self, document: S3PDocument) -> Path:
+        ...
+
+    @abstractmethod
+    def path_for(self, document: S3PDocument, asset: str) -> Path:
         ...
