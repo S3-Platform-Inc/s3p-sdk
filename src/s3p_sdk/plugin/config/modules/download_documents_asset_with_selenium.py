f077e064177d53e712abed42eff2eeda61dfc684
CuberHuber <lupashcko.rom@yandex.ru>
2025-04-04 11:22:18 +0300

    feature(module): to v0.2.12dev8 (#37)
    - add mandatory params for `DownloadDocumentsAssetWithSelenium` module

--- src/s3p_sdk/plugin/config/modules/download_documents_asset_with_selenium.py
+++ src/s3p_sdk/plugin/config/modules/download_documents_asset_with_selenium.py
@@ -9,9 +9,15 @@ from s3p_sdk.module import DownloadDocumentsAssetWithSelenium as MNAME
 class DownloadDocumentsAssetWithSelenium(AbcModuleConfig):
     """Модуль, который очищает строковые поля материала от мусорных символов"""
 
-    def __init__(self, order: int, is_critical: bool = False):
+    def __init__(self, order: int, available_field: str, is_critical: bool = False, cookie_selector: str = None):
         assert isinstance(is_critical, bool)
         assert isinstance(order, int) and order > 0
+        assert isinstance(available_field, str)
+        assert isinstance(cookie_selector, str) or cookie_selector is None
         self.order = order
         self.name = MNAME
         self.is_critical = is_critical
+        self.parameters = {
+            'available_field': available_field,
+            'cookie_selector': cookie_selector,
+        }
